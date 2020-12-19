abdbf4bf75c9fbb86be88eb28d2b55495756937a
t-tech <t.techa2020@gmail.com>
2020-12-19 03:16:38 +0000

    login signup

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,10 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  
   def index
-    @tasks = Task.all
+    if logged_in?
+      @tasks = Task.all
+    end
   end
 
   def show
@@ -12,7 +16,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に登録されました'
